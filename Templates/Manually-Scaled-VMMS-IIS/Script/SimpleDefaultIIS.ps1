b60f76c62463dd80e5cb5488f8b3c77561d95543
matousrokos <matousrokos@windowslive.com>
2016-02-29 21:48:54 +0100

    Adding LB and PS for WS Features

--- Templates/Manually-Scaled-VMMS-IIS/Script/SimpleDefaultIIS.ps1
+++ Templates/Manually-Scaled-VMMS-IIS/Script/SimpleDefaultIIS.ps1
@@ -1,2 +1,2 @@
 Import-Module ServerManager
-Install-WindowsFeature web-server,web-common-http,web-app-dev,web-asp-net45,web-appinit
+Install-WindowsFeature web-server,web-common-http,web-app-dev,web-asp-net45,web-appinit,web-mgmt-console
