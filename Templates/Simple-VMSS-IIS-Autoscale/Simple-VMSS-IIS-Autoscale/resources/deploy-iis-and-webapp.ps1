55f4f5497e411d91dd9b9e4c97ea0c84fc1edd53
Matous Rokos <matousrokos@outlook.com>
2017-01-10 18:21:27 +0000

    Initial Commit

---
+++ Templates/Simple-VMSS-IIS-Autoscale/Simple-VMSS-IIS-Autoscale/resources/deploy-iis-and-webapp.ps1
@@ -0,0 +1,53 @@
+﻿param(
+    [Parameter(Mandatory = $true)] 
+    [string]$FileName,
+    [Parameter(Mandatory = $true)] 
+    [string]$FileLocation
+)
+
+function ExitWithExitCode 
+{ 
+    param 
+    ( 
+        $ExitCode 
+    ) 
+
+    $host.SetShouldExit($ExitCode) 
+    exit 
+} 
+
+
+if ($FileLocation.EndsWith('/'))
+{
+    $zipfilesource = "$FileLocation$FileName"
+}
+else
+{
+    $zipfilesource = "$FileLocation/$FileName"
+}
+
+try
+{
+    Import-Module ServerManager
+    Install-WindowsFeature web-server,web-common-http,web-app-dev,web-asp-net45,web-appinit
+
+    $guid=[system.guid]::NewGuid().Guid
+    $folder="$env:temp\$guid"
+    New-Item -Path $folder -ItemType Directory
+
+    $zipfilelocal = "$folder\$filename"
+    $zipextracted="$folder\extracted"
+
+    Invoke-WebRequest $zipfilesource -OutFile $zipfilelocal
+
+    Add-Type -assembly "system.io.compression.filesystem"
+    [io.compression.zipfile]::ExtractToDirectory($zipfilelocal, $zipextracted)
+
+    copy-item $zipextracted\* c:\inetpub\wwwroot -Force -Recurse
+    ExitWithExitCode -ExitCode 0
+}
+catch
+{
+    Write-Error $_
+    ExitWithExitCode -ExitCode 1
+}
