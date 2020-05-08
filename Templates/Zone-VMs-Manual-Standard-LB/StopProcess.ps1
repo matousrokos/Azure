2d5b386d0861f772e971af95cb07c34c3ec3dbd3
matousrokos <matousrokos@outlook.com>
2020-05-08 15:02:51 +0200

    Update StopProcess.ps1

--- Templates/Zone-VMs-Manual-Standard-LB/StopProcess.ps1
+++ Templates/Zone-VMs-Manual-Standard-LB/StopProcess.ps1
@@ -1,2 +1,12 @@
-Stop-Process -Name Powel.CompositeWpf.Shells.WorkflowShell -force
-Stop-Process -Name RCEditor -force
+$RCEditor = Get-Process -Name RCEditor | select Id
+
+foreach ($Id in $RCE) { 
+Stop-Process -Name RCEditor -Force
+}
+
+$Nimbus = Get-Process -Name Powel.CompositeWpf.Shells.WorkflowShell | select Id
+
+
+foreach ($Id in $Nimbus) { 
+Stop-Process -Name Powel.CompositeWpf.Shells.WorkflowShell -Force
+}
